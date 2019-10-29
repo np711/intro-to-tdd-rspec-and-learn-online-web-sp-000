45f278623b0d833d200908262835fbf6bd392805
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-10-29 18:26:00 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,4 +1,8 @@
 require_relative '../current_age_for_birth_year.rb'
+puts "What your were you born?"
+gets. to_i 
+
+puts "You are:" +
 
 describe "current_age_for_birth_year method" do
   it "should return the current year for a person born in year 0" do
